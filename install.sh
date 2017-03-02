bad282966a40103057d59ecd27567d12bb270bdc
J. Bromley <jay@ai.co>
2017-03-02 12:52:46 -0800

    Modifed install for zgen.
    Changed from using raw oh-my-zsh to zgen for zsh configuration.

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ exit 0
 STOWARGS="-v -t ${HOME}"
 
 OH_MY_ZSH_DIR=${HOME}/.oh-my-zsh
+ZGEN_DIR=${HOME}/.zgen
 DOTFILES_DIR=${HOME}/.dotfiles
 
 # easy_packages="bash gdb git readline tmux executables pylint x"
@@ -28,6 +29,11 @@ install_oh_my_zsh() {
     ln -sf ${DOTFILES_DIR}/oh-my-zsh-custom ${OH_MY_ZSH_DIR}/custom
 }
 
+install_zgen() {
+    git clone https://github.com/tarjoilija/zgen.git "${ZGEN_DIR}"
+    ln -sf ${DOTFILES_DIR}/oh-my-zsh-custom "${ZGEN_DIR}/robbyrussell/oh-my-zsh-master/custom"
+}
+
 install_tpm() {
     mkdir ${HOME}/.tmux/plugins
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -38,7 +44,7 @@ for pkg in ${easy_packages}; do
     install_easy $pkg
 done
 
-install_oh_my_zsh
+install_zgen
 install_tpm
 
 pushd git && cp .gitconfig.template .gitconfig ; popd
