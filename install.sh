18554d4723625762c44d720c579100cf7f622102
J. Bromley <jay@ai.co>
2017-03-08 15:38:57 -0800

    Added code to isntall vim.
    Can install either minimal configuration or vim with Vundle and
    plugins. Also removed code to isntall oh-my-zsh, as this is
    automatically done by the zgen install.

--- install.sh
+++ install.sh
@@ -6,21 +6,18 @@ if [ ! -x ${STOW} ]; then
 fi
 STOWARGS="-v -t ${HOME}"
 
-OH_MY_ZSH_DIR=${HOME}/.oh-my-zsh
+VIM=/usr/bin/vim
+VIM_DIR=${HOME}/.vim
+VUNDLE_DIR=${VIM_DIR}/bundle/Vundle.vim 
+
 ZGEN_DIR=${HOME}/.zgen
 DOTFILES_DIR=${HOME}/.dotfiles
 
 # easy_packages="bash gdb git readline tmux executables pylint x"
-easy_packages="bash emacs git tmux x11 zsh"
+easy_packages="bash git tmux x11 zsh"
 
 install_easy() {
 	${STOW} ${STOWARGS} ${1}
-}
-
-install_oh_my_zsh() {
-    pushd ${HOME}
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    ln -sf ${DOTFILES_DIR}/oh-my-zsh-custom ${OH_MY_ZSH_DIR}/custom
 }
 
 install_zgen() {
@@ -33,6 +30,21 @@ install_tpm() {
     tmux run-shell /home/jay/.tmux/plugins/tpm/bindings/install_plugins
 }
 
+install_vim() {
+    install_easy vim_full
+    if [ -d ${VUNDLE_DIR} ]; then
+        pushd ${VUNDLE_DIR}
+        git pull
+        popd
+    else
+        git clone https://github.com/VundleVim/Vundle.vim.git ${VUNDLE_DIR}
+    fi  
+    ${VIM} +PluginInstall +qall
+    pushd ${VIM_DIR}/bundle/YouCompleteMe
+    ./install.py --clang-completer
+    popd
+}
+
 for pkg in ${easy_packages}; do
     install_easy $pkg
 done
@@ -40,6 +52,14 @@ done
 install_zgen
 install_tpm
 
-pushd git && cp .gitconfig.template .gitconfig ; popd
+# pushd git && cp .gitconfig.template .gitconfig ; popd
+
+if [ -x "$(which vim)" -a -x "$(which git)" ] && vim --version | grep -q +python; then
+    echo "Installing complete vim configuration."
+    install_vim
+else
+    echo "Installing simple vim configuration."
+    install_easy vim
+fi
 
 
