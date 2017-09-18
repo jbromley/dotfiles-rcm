6284f0c66e84fed7735857627aaa249963b26c06
J. Bromley <jay@ai.co>
2017-09-18 14:36:42 -0700

    Updated install file.
    Removed unused Vim configurations from the script and added to
    break in man pages .

--- install.sh
+++ install.sh
@@ -6,15 +6,11 @@ if [ ! -x ${STOW} ]; then
 fi
 STOWARGS="-v -t ${HOME}"
 
-VIM=/usr/bin/vim
-VIM_DIR=${HOME}/.vim
-VUNDLE_DIR=${VIM_DIR}/bundle/Vundle.vim 
-
 ZGEN_DIR=${HOME}/.zgen
 DOTFILES_DIR=${HOME}/.dotfiles
 
 # easy_packages="bash gdb git readline tmux executables pylint x"
-easy_packages="emacs git tmux x11 zsh"
+easy_packages="emacs git tmux x11 zsh misc"
 
 install_easy() {
         ${STOW} ${STOWARGS} ${1}
@@ -30,21 +26,6 @@ install_tpm() {
     tmux run-shell /home/jay/.tmux/plugins/tpm/bindings/install_plugins
 }
 
-install_vim() {
-    install_easy vim_full
-    if [ -d ${VUNDLE_DIR} ]; then
-        pushd ${VUNDLE_DIR}
-        git pull
-        popd
-    else
-        git clone https://github.com/VundleVim/Vundle.vim.git ${VUNDLE_DIR}
-    fi  
-    ${VIM} +PluginInstall +qall
-    pushd ${VIM_DIR}/bundle/YouCompleteMe
-    ./install.py --clang-completer
-    popd
-}
-
 for pkg in ${easy_packages}; do
     install_easy $pkg
 done
@@ -53,13 +34,4 @@ install_zgen
 install_tpm
 
 # pushd git && cp .gitconfig.template .gitconfig ; popd
-
-# if [ -x "$(which vim)" -a -x "$(which git)" ] && vim --version | grep -q +python; then
-#     echo "Installing complete vim configuration."
-#     install_vim
-# else
-#     echo "Installing simple vim configuration."
-#     install_easy vim
-# fi
-
 
