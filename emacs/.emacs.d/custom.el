2b3e5ae65590f651e48161015a8405c9fcaf0365
J. Bromley <jay@ai.co>
2017-12-08 10:36:23 -0800

    Emacs ANSI term color customizations.

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -3,6 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
  '(package-selected-packages
    (quote
     (color-theme-x tao-theme lenlen-theme solarized-theme eink-theme grayscale-theme helm-themes monochrome-theme naquadah-theme paper-theme plain-theme professional-theme white-theme hydandata-light-theme eziam-theme minimal-theme basic-theme atom-one-dark-theme rust-mode flatui-theme pomidor json-mode org org-bullets org-present htmlize inkpot-theme todotxt chess color-theme-sanityinc-tomorrow company ess magit markdown-mode polymode slime smooth-scrolling yaml-mode leuven-theme async helm use-package))))
