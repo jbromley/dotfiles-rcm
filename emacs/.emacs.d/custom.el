79d2f9821c6ffe34201c6230f7f517c3e977526a
J. Bromley <jay@ai.co>
2017-12-26 13:55:19 -0800

    Changed list of installed packages.
    Mostly theme changes are involved.

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -9,7 +9,7 @@
    ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
  '(package-selected-packages
    (quote
-    (color-theme-x tao-theme lenlen-theme solarized-theme eink-theme grayscale-theme helm-themes monochrome-theme naquadah-theme paper-theme plain-theme professional-theme white-theme hydandata-light-theme eziam-theme minimal-theme basic-theme atom-one-dark-theme rust-mode flatui-theme pomidor json-mode org org-bullets org-present htmlize inkpot-theme todotxt chess color-theme-sanityinc-tomorrow company ess magit markdown-mode polymode slime smooth-scrolling yaml-mode leuven-theme async helm use-package))))
+    (flatui-dark-theme color-theme-x tao-theme lenlen-theme solarized-theme eink-theme grayscale-theme helm-themes monochrome-theme naquadah-theme paper-theme plain-theme professional-theme white-theme hydandata-light-theme eziam-theme minimal-theme basic-theme atom-one-dark-theme rust-mode flatui-theme pomidor json-mode htmlize inkpot-theme todotxt chess color-theme-sanityinc-tomorrow company ess magit markdown-mode polymode slime smooth-scrolling yaml-mode leuven-theme async helm use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
