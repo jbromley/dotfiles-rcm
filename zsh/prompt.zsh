b86c02fb2509ed306172ecbc948f5253e110b0d0
J. Bromley <jay@ai.co>
2018-07-23 16:16:04 -0700

    Fixed up colors and prompt.
    Added variables for magenta and cyan for use in prompt definitions.
    Also changed the user name and machine in the prompt to use
    magenta.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,7 +3,7 @@
 if [[ "$TERM" != "dumb" ]] && [[ "$DISABLE_LS_COLORS" != "true" ]]; then
 #     PROMPT='[$(virtualenv_prompt_info)%{$fg[red]%}%n%{$reset_color%}@%{$fg[magenta]%}%m%{$reset_color%}:%{$fg[blue]%}%~%{$reset_color%}$(git_prompt_info)]
 # %# '
-    PROMPT='[$(virtualenv_prompt_info)%{$PR_BOLD_GREEN%}%n@%m%{$reset_color%}:%{$PR_BOLD_BLUE%}%~%{$reset_color%}$(git_prompt_info)]
+    PROMPT='[$(virtualenv_prompt_info)%{$PR_BOLD_MAGENTA%}%n@%m%{$reset_color%}:%{$PR_BOLD_BLUE%}%~%{$reset_color%}$(git_prompt_info)]
 %# '
 
     ZSH_THEME_GIT_PROMPT_PREFIX=" on %{$fg[yellow]%}"
