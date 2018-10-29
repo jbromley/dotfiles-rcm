e0f3178f75405c4778b551e7a0f61b6edd5c81ec
J. Bromley <jay@ai.co>
2018-10-29 13:29:15 -0700

    Change xmonad to blue/green theme.

--- tag-xmonad/xmonad/xmonad.hs
+++ tag-xmonad/xmonad/xmonad.hs
@@ -3,6 +3,7 @@ import XMonad.Actions.WindowBringer
 import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Util.Themes
 import XMonad.Layout.Decoration
 import XMonad.Layout.Square
 import XMonad.Layout.Tabbed
@@ -13,37 +14,42 @@ import Graphics.X11.ExtraTypes.XF86
 
 myManageHook = composeAll
     [ className =? "Spotify" --> doFloat
-    , resource =? "util_float" --> doFloat
+    , className =? "HTop" --> doFloat
     ]
 
-myTabConfig = def { activeBorderColor = "#FF0000"
-                  , activeColor = "#400000"
-                  , urgentBorderColor = "#FFA500"
-                  , urgentColor = "#402900"
+myTabConfig = def { activeBorderColor = "#0000FF"
+                  , activeColor = "#000040"
+                  , urgentBorderColor = "#00A5FF"
+                  , urgentColor = "#002940"
                   , fontName = "xft:DejaVu Sans Mono-8"
                   }
+
+myTallLayout = Tall 1 (1/100) (3/5)
+myTabbedLayout = tabbed shrinkText (theme kavonLakeTheme)
+myLayout = myTallLayout ||| myTabbedLayout ||| Full
               
 main = do
-    xmproc <- spawnPipe "/usr/bin/xmobar /home/jay/.config/xmobar/xmobarrc"
+    xmproc <- spawnPipe "/home/jay/.cabal/bin/xmobar /home/jay/.config/xmobar/xmobarrc"
     xmonad $ desktopConfig
         { manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
-        -- , layoutHook = avoidStruts $ layoutHook defaultConfig
-        , layoutHook = desktopLayoutModifiers $ Tall 1 (1/100) (3/5) ||| tabbed shrinkText myTabConfig ||| Full
+        , layoutHook = desktopLayoutModifiers $ myLayout
         , logHook = dynamicLogWithPP xmobarPP
-                        { ppCurrent = xmobarColor "orange" "" . wrap "[" "]"
+                        { ppCurrent = xmobarColor "cyan" "" . wrap "[" "]"
                         , ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "white" "" . shorten 50
                         }
         , modMask = mod4Mask
-        , borderWidth = 1
-        , terminal = "urxvtc"
-    	} `additionalKeys`
-        [ ((mod4Mask, xK_b), spawn "firefox")
+        , borderWidth = 2
+        , focusedBorderColor = "#0000FF"
+        , terminal = "urxvtc -e tmux"
+        } `additionalKeys`
+        [ ((mod4Mask, xK_p), spawn "rofi -show run")
+        , ((mod4Mask, xK_b), spawn "firefox")
         , ((mod4Mask, xK_d), spawn "evince")
         , ((mod4Mask, xK_f), spawn "nautilus --no-desktop")
         , ((mod4Mask .|. controlMask, xK_g), gotoMenu)
         , ((mod4Mask .|. controlMask, xK_b), bringMenu)
-        , ((mod4Mask .|. controlMask, xK_l), spawn "/home/jay/.local/bin/fuzzylock.sh")
+        , ((mod4Mask .|. controlMask, xK_l), spawn "i3lock -e -i /tmp/lockscreen.png")
         , ((0, xF86XK_AudioPlay), spawn "/home/jay/.local/bin/spcli play")
         , ((0, xF86XK_AudioNext), spawn "/home/jay/.local/bin/spcli next")
         , ((0, xF86XK_AudioPrev), spawn "/home/jay/.local/bin/spcli prev")
