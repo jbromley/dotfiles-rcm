d80470139346c80b825116e10e90233e9003f41a
J. Bromley <jay@ai.co>
2018-10-30 15:57:33 -0700

    Added scratchpads and exit confirmation.

--- tag-xmonad/xmonad/xmonad.hs
+++ tag-xmonad/xmonad/xmonad.hs
@@ -1,25 +1,36 @@
 import XMonad
+import XMonad.Actions.Submap
 import XMonad.Actions.WindowBringer
 import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.Tabbed
+import XMonad.ManageHook
+import XMonad.Prompt
+import XMonad.Prompt.ConfirmPrompt
+import XMonad.StackSet as W
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.NamedScratchpad
+import qualified Data.Map as M
 import Graphics.X11.ExtraTypes.XF86
+import System.Exit (exitWith, ExitCode(..))
 import System.IO
 
 main = do
   xmproc <- spawnPipe myStatusBar
   xmonad $ desktopConfig { terminal = myTerminal
                          , modMask = myMask
+                         , borderWidth = myBorderWidth
                          , layoutHook = desktopLayoutModifiers $ myLayoutHook
-                         , manageHook = myManageHook <+> manageDocks <+> manageHook defaultConfig
+                         , manageHook = myManageHook <+> manageHook defaultConfig
                          , logHook = dynamicLogWithPP $ myXmobarPP xmproc
                          } `additionalKeys` myKeys
 
 myTerminal = "urxvtc"
 myMask = mod4Mask
+myBorderWidth = 2
+myFont = "xft:Ubuntu Mono:style=Bold:size=10"
 myStatusBar = "/home/jay/.local/bin/xmobar"
 
 myLayoutHook = tiled ||| tabbedLayout ||| Full
@@ -34,16 +45,25 @@ myTabConfig = def { activeBorderColor = "#FF0000"
                   , activeColor = "#400000"
                   , urgentBorderColor = "#00A5FF"
                   , urgentColor = "#002940"
-                  , fontName = "xft:Ubuntu Mono-8"
+                  , fontName = myFont
                   , decoHeight = 32
                   }
 
+myScratchpads = [ NS "htop" "urxvtc -e htop" (title =? "htop") (customFloating $ W.RationalRect (1/4) (1/8) (1/2) (3/4))
+                , NS "calc" "urxvtc -e bc -l" (title =? "bc") (customFloating $ W.RationalRect 0 (3/4) (1/4) (1/4))
+                --, NS "spotify" "spotify" (className =? "Spotify") (customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3))
+                , NS "urxvt" "urxvtc -name scratch_urxvt" (resource =? "scratch_urxvt") (customFloating $ W.RationalRect (1/4) (1/4) (1/2) (1/2))
+                ]
+  
 myManageHook = composeAll
     [ className =? "spotify" --> doFloat
-    , resource =? "HTop" --> doFloat
+    , manageDocks
+    , namedScratchpadManageHook myScratchpads
     ]
 
-myXmobarPP h = xmobarPP { ppCurrent = xmobarColor "white" "#800000" . wrap " " " "
+myXmobarPP h = xmobarPP { ppCurrent = xmobarColor "white" "#a00000" . wrap "[" "]"
+                        , ppUrgent = xmobarColor "white" "#ffac00" . wrap ">" "<"
+                        , ppSep = " | "
                         , ppOutput = hPutStrLn h
                         , ppTitle = xmobarColor "white" "" . shorten 50
                         }
@@ -51,9 +71,16 @@ myXmobarPP h = xmobarPP { ppCurrent = xmobarColor "white" "#800000" . wrap " " "
 myKeys = [ ((myMask .|. controlMask, xK_Return), spawn "urxvtc -e tmux")
          , ((myMask, xK_p), spawn "rofi -show run")
          , ((myMask, xK_f), spawn "nautilus --no-desktop")
+         , ((myMask .|. shiftMask, xK_q), confirmPrompt myXPConfig "exit" $ io (exitWith ExitSuccess))
          , ((myMask .|. controlMask, xK_g), gotoMenu)
          , ((myMask .|. controlMask, xK_b), bringMenu)
          , ((myMask .|. controlMask, xK_l), spawn "i3lock -e -i /tmp/lockscreen.png")
+         , ((myMask, xK_s), submap . M.fromList $
+           [ ((0, xK_c), namedScratchpadAction myScratchpads "calc")
+           , ((0, xK_h), namedScratchpadAction myScratchpads "htop")
+           , ((0, xK_s), namedScratchpadAction myScratchpads "spotify")
+           , ((0, xK_t), namedScratchpadAction myScratchpads "urxvt")
+           ])
          , ((0, xF86XK_AudioPlay), spawn "/home/jay/.local/bin/spcli play")
          , ((0, xF86XK_AudioNext), spawn "/home/jay/.local/bin/spcli next")
          , ((0, xF86XK_AudioPrev), spawn "/home/jay/.local/bin/spcli prev")
@@ -63,3 +90,11 @@ myKeys = [ ((myMask .|. controlMask, xK_Return), spawn "urxvtc -e tmux")
          , ((0, xF86XK_HomePage), spawn "/home/jay/.local/bin/xhtop")
         ]
 
+myXPConfig :: XPConfig
+myXPConfig = def { font = myFont
+                 , height = 24
+                 , borderColor = "#FF0000"
+                 , bgColor = "#C00000"
+                 , fgColor = "#FFFFFF"
+                 , position = CenteredAt (1/2) (3/16)
+                 }
