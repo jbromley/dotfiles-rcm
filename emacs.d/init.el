722478aa3186d0c52b01ca3c1172654be8c43d01
J. Bromley <jay@ai.co>
2019-05-14 14:54:41 -0700

    Fixed problem loading use-package.
    On the initial run of a new Emacs, use-package was not found and
    causing an error. Added code to properly install use-package.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -17,8 +17,9 @@
 
 ;; Use use-package to install/manage packages.
 (unless (package-installed-p 'use-package)
+  (package-refresh-contents)
   (package-install 'use-package))
-(require 'use-package)
+(eval-when-compile (require 'use-package))
 (setq use-package-always-ensure t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
