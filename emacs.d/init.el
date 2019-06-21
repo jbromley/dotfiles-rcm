35204eb79f0bf882da17da7c55f1e83c81300494
J. Bromley <jay@ai.co>
2019-06-21 15:12:00 -0700

    Configured Emacs for Clojure and CIDER.
    Added packages and configuration to develop Clojure.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -47,6 +47,10 @@
 (setq-default fill-column 80)
 (setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
 
+;; Integrate with system clipboard and selection.
+(setq select-enable-primary t
+      select-enable-clipboard t)
+      
 ;; Set the default formats for modes derived from cc-mode.
 (setq c-default-style (quote
                        ((c-mode . "k&r")
@@ -195,6 +199,61 @@
 ;; Projectile mode for project management
 (use-package projectile
   :bind (:map projectile-mode-map ("C-c p" . projectile-command-map)))
+
+;; Clojure
+
+(use-package paredit)
+
+(use-package enh-ruby-mode)
+
+(use-package clojure-mode-extra-font-locking)
+
+(use-package clojure-mode
+  :config
+  (add-hook 'clojure-mode-hook 'enable-paredit-mode)
+  (add-hook 'clojure-mode-hook 'subword-mode)
+  (add-hook 'clojure-mode-hook
+	    (lambda ()
+	      (setq inferior-lisp-program "lein repl")
+	      (font-lock-add-keywords
+	       nil
+	       '(("(\\(facts?\\)" (1 font-lock-keyword-face))
+		 ("(\\(background?\\)" (1 font-lock-keyword-face))))
+	      (define-clojure-indent (fact 1))
+	      (define-clojure-indent (facts 1))))
+  (add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
+  (add-to-list 'auto-mode-alist '("\\.boot$" . clojure-mode))
+  (add-to-list 'auto-mode-alist '("\\.cljs.*$" . clojure-mode))
+  (add-to-list 'auto-mode-alist '("lein-env" . enh-ruby-mode)))
+
+(use-package cider
+  :config
+  (add-hook 'cider-mode-hook 'eldoc-mode)
+  (setq cider-repl-pop-to-buffer-on-connect t
+	cider-show-error-buffer t
+	cider-auto-select-error-buffer t
+	cider-repl-history-file "~/.emacs.d/cider-history"
+	cider-repl-wrap-history t)
+  (defun cider-start-http-server ()
+    (interactive)
+    (cider-load-current-buffer)
+    (let ((ns (cider-current-ns)))
+      (cider-repl-set-ns ns)
+      (cider-interactive-eval (format "(println '(def server (%s/start))) (println 'server)" ns))
+      (cider-interactive-eval (format "(def server (%s/start)) (println server)" ns))))
+  (defun cider-refresh ()
+    (interactive)
+    (cider-interactive-eval (format "(user/reset)")))
+  (defun cider-user-ns ()
+    (interactive)
+    (cider-repl-set-ns "user"))
+  :bind
+  (:map clojure-mode-map
+	("C-c C-v" . cider-start-http-server)
+	("C-M-r" . cider-refresh)
+	("C-c u" . cider-user-ns))
+  (:map cider-mode-map 
+	("C-c u" . cider-user-ns)))
 	      
 ;; Theme switching
 (use-package theme-looper
