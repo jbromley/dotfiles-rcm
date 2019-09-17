95a151acc71bd5179e0209463664674453523485
J. Bromley <jay@ai.co>
2019-09-17 14:51:03 -0700

    Use C-enter for eval-print-last-sexp.
    When using paredit mode with the lisp-interaction-mode, C-j no longer
    evaluates the sexp, but rather just calls paredit-newline. Bound C-enter
    to eval-print-last-sexp to be able to evaluate lisp forms.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -206,7 +206,10 @@
   :config
   (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
   (add-hook 'lisp-mode-hook 'enable-paredit-mode)
-  (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode))
+  (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+  (add-hook 'lisp-interaction-mode-hook (lambda ()
+					  (enable-paredit-mode)
+					  (local-set-key [C-return] 'eval-print-last-sexp))))
 
 
 (use-package enh-ruby-mode)
