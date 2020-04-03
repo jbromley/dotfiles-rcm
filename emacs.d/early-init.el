6787f031109d7ce9807cfc0bb47eaf52ca3bcb9f
J. Bromley <jay@ai.co>
2020-04-02 18:21:03 -0700

    Only set frame size for MacOS.

--- emacs.d/early-init.el
+++ emacs.d/early-init.el
@@ -1,9 +1,11 @@
 ;;;; early-init.el
 
+;;; On MacOS set a default frame size.
+(if (string-equal system-tyle "darwin")
+    (progn
+      (add-to-list 'default-frame-alist '(width . 164))
+      (add-to-list 'default-frame-alist '(height . 80))))
+
 ;;; Remove graphical items we do not use.
-(add-to-list 'default-frame-alist '(width . 164))
-(add-to-list 'default-frame-alist '(height . 60))
 (add-to-list 'default-frame-alist '(tool-bar-lines . 0))
 (add-to-list 'default-frame-alist '(vertical-scroll-bars))
-
-
