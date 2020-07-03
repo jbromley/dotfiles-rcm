2c966bff0e6155dace3a7a3b621e8cff60c8c574
J. Bromley <jay@ai.co>
2020-07-03 01:49:37 -0700

    Added fish configuration to repo.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,1 +1,10 @@
-set PATH $HOME/.local/bin $PATH
+# Add our local bin directory to the path.
+set PATH "/home/jay/.local/bin:$PATH"
+# Set the default editor.
+set -U EDITOR vim
+
+# Use new fzf key bindings.
+set -U FZF_LEGACY_KEYBINDINGS 0
+
+starship init fish | source
+
