610f123d503d244a8270061d13ef350dad0de55e
J. Bromley <jay@ai.co>
2020-07-10 11:26:02 -0700

    Change fish to all-ASCII pure prompt.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,5 +6,10 @@ set -U EDITOR vim
 # Use new fzf key bindings.
 set -U FZF_LEGACY_KEYBINDINGS 0
 
+# Prompt configuration
 # starship init fish | source
+set -g pure_symbol_prompt ">"
+set -g pure_symbol_reverse_prompt "<"
+set -g pure_symbol_unpulled_commits "v"
+set -g pure_symbol_unpushed_commits "^"
 
