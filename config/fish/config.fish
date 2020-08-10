6ce8743e69e1764651be39e0cca3a84ad824d635
J. Bromley <jay@ai.co>
2020-08-10 10:05:52 -0700

    Removed unnecessary env variables.
    Universal variables don't need to be set at every launch. Removed all
    "set -U" calls. Also removed variables for Pure shell prompt
    configuration.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,15 +1,2 @@
-# Add our local bin directory to the path.
-set PATH "/home/jay/.local/bin:$PATH"
-# Set the default editor.
-set -U EDITOR nvim
-
-# Use new fzf key bindings.
-set -U FZF_LEGACY_KEYBINDINGS 0
-
-# Prompt configuration
-# starship init fish | source
-set -g pure_symbol_prompt ">"
-set -g pure_symbol_reverse_prompt "<"
-set -g pure_symbol_unpulled_commits "v"
-set -g pure_symbol_unpushed_commits "^"
-
+# Show virtualenv name.
+set -g tide_virtual_env_display_mode 'venvName'
