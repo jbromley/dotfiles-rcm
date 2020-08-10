8983dbd605c27c993f9885db381adc197feddb89
Jay Bromley <jbromley@gmail.com>
2020-08-10 02:04:35 -0700

    Added asdf set up and completion.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,15 +1,6 @@
 # Add our local bin directory to the path.
-set PATH "/home/jay/.local/bin:$PATH"
-# Set the default editor.
-set -U EDITOR nvim
+set -g --prepend PATH $HOME/.local/bin
 
-# Use new fzf key bindings.
-set -U FZF_LEGACY_KEYBINDINGS 0
-
-# Prompt configuration
-# starship init fish | source
-set -g pure_symbol_prompt ">"
-set -g pure_symbol_reverse_prompt "<"
-set -g pure_symbol_unpulled_commits "v"
-set -g pure_symbol_unpushed_commits "^"
-
+# Set up asdf.
+source ~/.asdf/asdf.fish
+source ~/.config/fish/completions/asdf.fish
