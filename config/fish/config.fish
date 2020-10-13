3a3387422dcdd797eaf686073dcbe6bb55119300
J. Bromley <jay@ai.co>
2020-10-13 14:08:33 -0700

    Removed commented code. Set up Go env.
    Removed commented code for setting paths - now Using user_file_paths.
    Added global environment variables for GOPATH and GOBIN.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,11 @@
-# Add our local bin directory to the path.
-#set -g --prepend PATH $HOME/.local/bin
-
 # Show virtualenv name.
 set -g tide_virtual_env_display_mode 'venvName'
 
 # Set up asdf.
 source ~/.asdf/asdf.fish
 source ~/.config/fish/completions/asdf.fish
+
+# Set up Go directories.
+set --global --export GOPATH ~/go
+set --global --export GOBIN $GOPATH/bin
+
