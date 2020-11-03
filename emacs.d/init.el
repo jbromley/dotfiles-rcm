7342c0f268ff6be40c9e844bda8f8cf381074157
J. Bromley <jay@ai.co>
2020-11-03 02:12:52 -0800

    Remove custom variables from main config.
    custom-file was not properly set so customizations were being written
    into init.el. Fixed so that customizations are recorded in their own
    file.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -274,3 +274,18 @@
 
 
 (put 'narrow-to-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("37144b437478e4c235824f0e94afa740ee2c7d16952e69ac3c5ed4352209eefb" default))
+ '(package-selected-packages
+   '(theme-looper spacemacs-theme modus-vivendi-theme modus-operandi-theme oceanic-theme doom-themes slime yaml-mode go-mode org-superstar lsp-mode counsel-projectile counsel ivy smooth-scrolling fzf ag company treemacs-magit treemacs-icons-dired treemacs-projectile treemacs which-key use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
