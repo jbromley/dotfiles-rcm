86393eb0f1c0bf6eed114cd4db44e2f52af7ce5a
J. Bromley <jbromley@gmail.com>
2021-02-09 18:08:43 -0800

    Set default frame size in early-init.el.

--- config/emacs/early-init.el
+++ config/emacs/early-init.el
@@ -3,3 +3,5 @@
 ;;; Remove graphical items we do not use.
 (add-to-list 'default-frame-alist '(tool-bar-lines . 0))
 (add-to-list 'default-frame-alist '(vertical-scroll-bars))
+(add-to-list 'default-frame-alist '(width . 100))
+(add-to-list 'default-frame-alist '(height . 50))
