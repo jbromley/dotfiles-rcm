969ea5daf50f4916ca84691453b4fb68ba0af4a5
J. Bromley <jay@appliedinvention.com>
2021-02-18 16:26:00 -0800

    Remove KDE code to switch workspaces.
    The Ubuntu VM script had a line that caused KDE to switch to a new
    workspace before starting the virtual machine. Removed the line so the
    script will work in GNOME.

--- local/bin/ubuntu-vm.sh
+++ local/bin/ubuntu-vm.sh
@@ -1,6 +1,4 @@
 #! /usr/bin/env bash
 # Launch the Windows development VM
-qdbus org.kde.KWin /KWin setCurrentDesktop 2
-virsh --connect qemu:///system start ubuntu20.04
-virt-viewer --connect=qemu:///system --wait --full-screen ubuntu20.04
-
+virsh --connect qemu:///system start ubuntu-20.04
+virt-viewer --connect=qemu:///system --wait --full-screen ubuntu-20.04
