99b8ced11fca42507b02b85bcadc8bf8635040fb
J. Bromley <jbromley@gmail.com>
2021-05-07 22:14:37 -0700

    Added wezterm configuration to dotfiles.

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require 'wezterm';
+
+return {
+    font = wezterm.font("Fira Code"),
+    font_size = 11.0,
+
+    color_scheme = "Dracula",
+
+    tab_max_width = 24,
+    tab_bar_at_bottom = true,
+
+    window_decorations = "NONE",
+}
