9d76129612a0f446b028dea81c3f8e345b6ff0bc
J. Bromley <jay@appliedinvention.com>
2021-05-13 10:05:51 -0700

    Added wezterm configuration.

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm';
+
+return {
+    font = wezterm.font("Fira Code"),
+    font_size = 11.0,
+
+    color_scheme = "Dracula",
+
+    tab_max_width = 24,
+    tab_bar_at_bottom = true,
+
+    window_decorations = "NONE",
+    window_background_opacity = 0.875,
+}
