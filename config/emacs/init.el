e46ad0241af695d39fcd7427f1d497861f3a30fd
J. Bromley <jay@appliedinvention.com>
2021-05-25 17:40:00 -0700

    Set up ligatures for Emacs.
    Works with Fira Code and Iosevka.

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -27,7 +27,8 @@
 (setq use-package-always-ensure t)
 
 ;; Add custom Emacs Lisp directory to the load path.
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/elisp"))
+(let ((default-directory (expand-file-name "~/.config/emacs/elisp")))
+  (normal-top-level-add-subdirs-to-load-path))
 
 ;; Set large file size limits
 (setq large-file-warning-threshold 16000000)
@@ -114,7 +115,8 @@
 
 ;; All the icons
 (use-package all-the-icons)
-(use-package all-the-icons-dired)
+(use-package all-the-icons-dired
+  :hook (dired-mode . all-the-icons-dired-mode))
 
 ;; Treemacs
 (use-package treemacs
@@ -210,6 +212,27 @@
   :bind (("C-x g" . magit-status)
          ("C-x M-g" . magit-dispatch)
          ("C-c g" . magit-file-dispatch)))
+
+;; Ligatures
+(use-package ligature
+  :ensure nil
+  :config
+  (ligature-set-ligatures 't '("www"))
+  (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
+  (ligature-set-ligatures 'prog-mode '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
+                                       ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
+                                       "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
+                                       "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
+                                       "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
+                                       "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
+                                       "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
+                                       "[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
+                                       ">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
+                                       "<$" "<=" "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
+                                       "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
+                                       "?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
+                                       "\\\\" "://"))
+  (global-ligature-mode t))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Languages and file format packages
@@ -350,6 +373,11 @@ link to the JIRA issue."
          (racket-repl-mode . enable-paredit-mode)
          (scheme-mode . enable-paredit-mode)))
 
+;; Lisp interaction mode
+(add-hook 'lisp-interaction-mode-hook
+          (lambda ()
+            (define-key lisp-interaction-mode-map (kbd "C-<return>") 'eval-print-last-sexp)))
+
 ;; Common Lisp - SLY
 (use-package sly
   :custom
@@ -402,9 +430,8 @@ link to the JIRA issue."
   :init
   (setq modus-themes-slanted-constructs t
         modus-themes-bold-constructs t)
-  (modus-themes-load-themes)
-  :bind
-  (("<f5>" . modus-themes-toggle)))
+  (modus-themes-load-themes))
+
 
 (use-package moe-theme
   :defer t)
@@ -420,8 +447,6 @@ link to the JIRA issue."
   (theme-looper-set-favorite-themes '(*default*
                                       dichromacy
                                       misterioso
-                                      tsdh-dark
-                                      tsdh-light
                                       dracula
                                       modus-operandi
                                       modus-vivendi
