1b196518f28afc6466f2c242ffdc17b5d69d375b
J. Bromley <jay@appliedinvention.com>
2021-07-09 16:59:42 -0700

    Added chemacs2 profiles.
    Added profiles to run my plain Emacs configuration, Doom Emacs, and
    Spacemacs.

---
+++ emacs-profiles.el
@@ -0,0 +1,6 @@
+(("default"   . ((user-emacs-directory . "~/.config/emacs-plain")))
+ ("doom"      . ((user-emacs-directory . "~/.config/emacs-doom")
+	         (env . (("DOOMDIR"    . "~/.config/doom")))))
+ ("spacemacs" . ((user-emacs-directory . "~/.config/emacs-spacemacs")
+		 (env . (("SPACEMACSDIR" . "~/.config/spacemacs")))))))
+
