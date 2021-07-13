aab1e9bf9b2cc74927e3b102413c85916cf098c0
J. Bromley <jay@appliedinvention.com>
2021-07-12 22:18:12 -0700

    Changed "default" to "plain" in Chemacs.
    To be able to more easily change default profiles in Chemacs2, changed
    the label for the plain Emacs configuration from "default" to "plain".

--- emacs-profiles.el
+++ emacs-profiles.el
@@ -1,6 +1,5 @@
-(("default"   . ((user-emacs-directory . "~/.config/emacs-plain")))
- ("doom"      . ((user-emacs-directory . "~/.config/emacs-doom")
-	         (env . (("DOOMDIR"    . "~/.config/doom")))))
- ("spacemacs" . ((user-emacs-directory . "~/.config/emacs-spacemacs")
-		 (env . (("SPACEMACSDIR" . "~/.config/spacemacs")))))))
-
+(("plain"     . ((user-emacs-directory   . "~/.config/emacs-plain")))
+ ("doom"      . ((user-emacs-directory   . "~/.config/emacs-doom")
+                 (env . (("DOOMDIR"      . "~/.config/doom")))))
+ ("spacemacs" . ((user-emacs-directory   . "~/.config/emacs-spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/.config/spacemacs")))))))
