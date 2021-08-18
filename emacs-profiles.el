f127d90fe0cf28baaf660dd99225c70626ce2f71
J. Bromley <jay@appliedinvention.com>
2021-08-18 15:25:25 -0700

    Deleted Chemacs2 configuration files.

--- emacs-profiles.el
+++
@@ -1,5 +0,0 @@
-(("plain"     . ((user-emacs-directory   . "~/.config/emacs-plain")))
- ("doom"      . ((user-emacs-directory   . "~/.config/emacs-doom")
-                 (env . (("DOOMDIR"      . "~/.config/doom")))))
- ("spacemacs" . ((user-emacs-directory   . "~/.config/emacs-spacemacs")
-                 (env . (("SPACEMACSDIR" . "~/.config/spacemacs")))))))
