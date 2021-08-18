9a6711dceee47c676471c3474fd8c770bb1365d6
J. Bromley <jay@appliedinvention.com>
2021-08-18 15:23:35 -0700

    Make emacsclient not wait for new frame.
    Added the --no-wait flag to the 'e' alias for emacsclient. This way the terminal
    becomes available immediately after Emacs starts instead of waiting for the user
    to exit Emacs.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -12,7 +12,7 @@ set --global --export GOBIN $GOPATH/bin
 # Aliases
 alias xo=xdg-open
 alias et='emacsclient --tty'
-alias e='emacsclient --create-frame'
+alias e='emacsclient --create-frame --no-wait'
 
 # Configure fzf.
 set --export FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=75% --preview-window=right:60%:wrap --marker="*"'
