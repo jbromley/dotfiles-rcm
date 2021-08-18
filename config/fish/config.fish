165d4490fa3e31fd583330ecf9c030dfb83f7e59
J. Bromley <jay@appliedinvention.com>
2021-08-18 16:43:16 -0700

    Added fish alias for batcat.
    View files with "b".

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -10,9 +10,10 @@ set --global --export GOPATH ~/go
 set --global --export GOBIN $GOPATH/bin
 
 # Aliases
-alias xo=xdg-open
+alias b=batcat
 alias et='emacsclient --tty'
 alias e='emacsclient --create-frame --no-wait'
+alias xo=xdg-open
 
 # Configure fzf.
 set --export FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=75% --preview-window=right:60%:wrap --marker="*"'
