8b6e7c37edc6bddedf772f8082ba4073a7df71ab
J. Bromley <jay@appliedinvention.com>
2021-08-27 10:57:57 -0700

    User Leader key (Caps Lock) in key bindings.
    Too many control keys were being used for WezTerm bindings. Disabled
    Caps Lock and used this as a leader key. Remapped tab and pane actions
    to use the leader key instead of Super, Ctrl, Alt, etc.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -5,13 +5,12 @@ local my_keys = {
 
     -- Copy and paste
     {key = "c", mods = "CTRL|SHIFT", action = wezterm.action{CopyTo="ClipboardAndPrimarySelection"}},
-    {key = "v", mods = "CTRL|SHIFT", action = wezterm.action{PasteFrom="PrimarySelection"}},
+    {key = "v", mods = "CTRL|SHIFT", action = wezterm.action{PasteFrom="Clipboard"}},
     {key = "x", mods="CTRL|SHIFT", action = "ActivateCopyMode"},
     {key = " ", mods="CTRL|SHIFT", action = "QuickSelect"},
 
     -- Search
     {key = "f", mods="CTRL|SHIFT", action = wezterm.action{Search = {CaseInSensitiveString = ""}}}, 
-    {key = "Enter", mods = "ALT", action = "ToggleFullScreen"}, 
 
     -- Font size adjustment
     {key = "-", mods = "CTRL", action = "DecreaseFontSize"},
@@ -19,19 +18,27 @@ local my_keys = {
     {key = "0", mods = "CTRL", action = "ResetFontSize"},
 
     -- New windows and tab creation, closing, and navigation
-    {key = "n", mods = "CTRL|SHIFT", action = "SpawnWindow"}, 
-    {key = "t", mods = "CTRL|SHIFT", action = wezterm.action{SpawnTab = "CurrentPaneDomain"}},
-    {key = "t", mods = "CTRL|ALT", action = wezterm.action{SpawnTab = "DefaultDomain"}},
-    {key = "w", mods = "CTRL|SHIFT", action = wezterm.action{CloseCurrentTab = {confirm = true}}},
-    {key = "[", mods = "SUPER|ALT", action = wezterm.action{ActivateTabRelative = -1}},
-    {key = "]", mods = "SUPER|ALT", action = wezterm.action{ActivateTabRelative = 1}},
-    {key = "p", mods = "LEADER", action = wezterm.action{ActivateTabRelative = -1}},
-    {key = "n", mods = "LEADER", action = wezterm.action{ActivateTabRelative = 1}},
-    {key = "o", mods = "SUPER|ALT", action = "ActivateLastTab"},
+    -- {key = "n", mods = "CTRL|SHIFT", action = "SpawnWindow"}, 
+    {key = "w", mods = "LEADER", action = "SpawnWindow"},
+    -- {key = "t", mods = "CTRL|SHIFT", action = wezterm.action{SpawnTab = "CurrentPaneDomain"}},
+    {key = "t", mods = "LEADER", action = wezterm.action{SpawnTab = "CurrentPaneDomain"}},
+    -- {key = "t", mods = "CTRL|ALT", action = wezterm.action{SpawnTab = "DefaultDomain"}},
+    {key = "T", mods = "LEADER", action = wezterm.action{SpawnTab = "DefaultDomain"}},
+    -- {key = "w", mods = "CTRL|SHIFT", action = wezterm.action{CloseCurrentTab = {confirm = true}}},
+    {key = "x", mods = "LEADER", action = wezterm.action{CloseCurrentTab = {confirm = true}}},
+    -- {key = "[", mods = "SUPER|ALT", action = wezterm.action{ActivateTabRelative = -1}},
+    -- {key = "]", mods = "SUPER|ALT", action = wezterm.action{ActivateTabRelative = 1}},
+    {key = "[", mods = "LEADER", action = wezterm.action{ActivateTabRelative = -1}},
+    {key = "]", mods = "LEADER", action = wezterm.action{ActivateTabRelative = 1}},
+    -- {key = "o", mods = "SUPER|ALT", action = "ActivateLastTab"},
     {key = "o", mods = "LEADER", action = "ActivateLastTab"},
-    {key = "[", mods = "CTRL|SUPER|ALT", action = wezterm.action{MoveTabRelative = -1}},
-    {key = "]", mods = "CTRL|SUPER|ALT", action = wezterm.action{MoveTabRelative = 1}},
-    {key = "n", mods = "SUPER|ALT", action = "ShowTabNavigator"},
+    -- {key = "[", mods = "CTRL|SUPER|ALT", action = wezterm.action{MoveTabRelative = -1}},
+    -- {key = "]", mods = "CTRL|SUPER|ALT", action = wezterm.action{MoveTabRelative = 1}},
+    {key = "{", mods = "LEADER", action = wezterm.action{MoveTabRelative = -1}},
+    {key = "}", mods = "LEADER", action = wezterm.action{MoveTabRelative = 1}},
+    -- {key = "n", mods = "SUPER|ALT", action = "ShowTabNavigator"},
+    {key = "n", mods = "LEADER", action = "ShowTabNavigator"},
+    {key = "Enter", mods = "ALT", action = "ToggleFullScreen"}, 
 
     -- Scrollback buffer
     {key = "PageUp", mods = "SHIFT", action = wezterm.action{ScrollByPage = -1}},
@@ -39,24 +46,24 @@ local my_keys = {
     {key = "k", mods="CTRL|SHIFT", action = wezterm.action{ClearScrollback = "ScrollbackOnly"}},
 
     -- Pane creation, resizing, and navigation
-    {key = "|", mods="SUPER", action = wezterm.action{SplitHorizontal = {domain = "CurrentPaneDomain"}}},
-    {key = "-", mods="SUPER", action = wezterm.action{SplitVertical = {domain = "CurrentPaneDomain"}}},
-    {key = "LeftArrow", mods="SHIFT", action = wezterm.action{ActivatePaneDirection = "Left"}},
-    {key = "RightArrow", mods="SHIFT", action = wezterm.action{ActivatePaneDirection = "Right"}},
-    {key = "UpArrow", mods="SHIFT", action = wezterm.action{ActivatePaneDirection = "Up"}},
-    {key = "DownArrow", mods="SHIFT", action = wezterm.action{ActivatePaneDirection = "Down"}},
-    {key = "LeftArrow", mods="SUPER|ALT", action = wezterm.action{AdjustPaneSize = {"Left", 1}}},
-    {key = "RightArrow", mods="SUPER|ALT", action = wezterm.action{AdjustPaneSize = {"Right", 1}}},
-    {key = "UpArrow", mods="SUPER|ALT", action = wezterm.action{AdjustPaneSize = {"Up", 1}}},
-    {key = "DownArrow", mods="SUPER|ALT", action = wezterm.action{AdjustPaneSize = {"Down", 1}}},
-    {key = "z", mods = "SUPER", action = "TogglePaneZoomState"},
+    {key = "|", mods="LEADER", action = wezterm.action{SplitHorizontal = {domain = "CurrentPaneDomain"}}},
+    {key = "-", mods="LEADER", action = wezterm.action{SplitVertical = {domain = "CurrentPaneDomain"}}},
+    {key = "h", mods="LEADER", action = wezterm.action{ActivatePaneDirection = "Left"}},
+    {key = "l", mods="LEADER", action = wezterm.action{ActivatePaneDirection = "Right"}},
+    {key = "k", mods="LEADER", action = wezterm.action{ActivatePaneDirection = "Up"}},
+    {key = "j", mods="LEADER", action = wezterm.action{ActivatePaneDirection = "Down"}},
+    {key = "LeftArrow", mods="SHIFT", action = wezterm.action{AdjustPaneSize = {"Left", 1}}},
+    {key = "RightArrow", mods="SHIFT", action = wezterm.action{AdjustPaneSize = {"Right", 1}}},
+    {key = "UpArrow", mods="SHIFT", action = wezterm.action{AdjustPaneSize = {"Up", 1}}},
+    {key = "DownArrow", mods="SHIFT", action = wezterm.action{AdjustPaneSize = {"Down", 1}}},
+    {key = "z", mods = "LEADER", action = "TogglePaneZoomState"},
 
     -- Debugging
     {key = "F2", mods = "CTRL", action = "ShowDebugOverlay"}, 
 }
 
 for i = 1, 8 do
-    table.insert(my_keys, {key = tostring(i), mods = "SUPER|ALT", action = wezterm.action{ActivateTab = i - 1}})
+    table.insert(my_keys, {key = tostring(i), mods = "LEADER", action = wezterm.action{ActivateTab = i - 1}})
 end
 
 return {
