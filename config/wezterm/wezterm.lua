c3b6db2729f514e699908addc7588826d072fd32
J. Bromley <jay@appliedinvention.com>
2021-09-03 01:52:42 -0700

    Configured WezTerm tab bar.
    Use Dracula Pro colors in the tab bar.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -68,14 +68,91 @@ for i = 1, 8 do
     table.insert(my_keys, {key = tostring(i), mods = "LEADER", action = wezterm.action{ActivateTab = i - 1}})
 end
 
+local my_tab_bar = {
+   -- The color of the strip that goes along the top of the window
+   --background = "#2a2a80",
+   background = "#000040",
+
+      -- The active tab is the one that has focus in the window
+      active_tab = {
+        -- The color of the background area for the tab
+        bg_color = "#5555ff",
+        -- The color of the text for the tab
+        fg_color = "#fcfcff",
+
+        -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+        -- label shown for this tab.
+        -- The default is "Normal"
+        intensity = "Bold",
+
+        -- Specify whether you want "None", "Single" or "Double" underline for
+        -- label shown for this tab.
+        -- The default is "None"
+        underline = "None",
+
+        -- Specify whether you want the text to be italic (true) or not (false)
+        -- for this tab.  The default is false.
+        italic = false,
+
+        -- Specify whether you want the text to be rendered with strikethrough (true)
+        -- or not for this tab.  The default is false.
+        strikethrough = false,
+      },
+
+      -- Inactive tabs are the tabs that do not have focus
+      inactive_tab = {
+        bg_color = "#2a2a80",
+        fg_color = "#babacc",
+
+        -- The same options that were listed under the `active_tab` section above
+        -- can also be used for `inactive_tab`.
+        intensity = "Normal",
+      },
+
+      -- You can configure some alternate styling when the mouse pointer
+      -- moves over inactive tabs
+      inactive_tab_hover = {
+        bg_color = "#5455e0",
+        fg_color = "#c8c8c8",
+        italic = true,
+
+        -- The same options that were listed under the `active_tab` section above
+        -- can also be used for `inactive_tab_hover`.
+      },
+
+      -- The new tab button that let you create new tabs
+      new_tab = {
+        bg_color = "#4040c0",
+        fg_color = "#fcfcff",
+
+        -- The same options that were listed under the `active_tab` section above
+        -- can also be used for `new_tab`.
+      },
+
+      -- You can configure some alternate styling when the mouse pointer
+      -- moves over the new tab button
+      new_tab_hover = {
+        bg_color = "#5555ff",
+        fg_color = "#fcfcff",
+        italic = true,
+
+        -- The same options that were listed under the `active_tab` section above
+        -- can also be used for `new_tab_hover`.
+      }
+}
+
 return {
     font = wezterm.font("JetBrainsMono Nerd Font"),
     font_size = 11.0,
 
     color_scheme = "Dracula Pro",
 
+    colors = {
+       tab_bar = my_tab_bar,
+    },
+
     tab_max_width = 32,
-    tab_bar_at_bottom = true,
+    tab_bar_at_bottom = false,
 
     window_decorations = "NONE",
     window_background_opacity = 0.875,
