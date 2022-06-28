2d9c84e2bdf52c91ff422d1bbf524323d5a167a7
J. Bromley <jay@appliedinvention.com>
2022-06-28 12:56:57 -0700

    Make Emacs startup width 104 characters.

--- emacs.d/early-init.el
+++ emacs.d/early-init.el
@@ -11,7 +11,7 @@
 (add-to-list 'default-frame-alist '(vertical-scroll-bars))
 
 ;; Set default frame size.
-(add-to-list 'default-frame-alist '(width . 100))
+(add-to-list 'default-frame-alist '(width . 104))
 (add-to-list 'default-frame-alist '(height . 50))
 
 (provide 'early-init)
