0e171f2cd8c2bd27b75ccdc622d6ff5e2f4ed3da
J. Bromley <jay@appliedinvention.com>
2023-10-27 13:36:44 -0700

    Removed Copilot from Neovim configuration

--- config/nvim/lua/plugins/plugins.lua
+++ config/nvim/lua/plugins/plugins.lua
@@ -25,13 +25,13 @@ return {
   {
     "gleam-lang/gleam.vim",
   },
-  {
-    "github/copilot.vim",
-    init = function(_)
-      vim.g.copilot_no_tab_map = true
-      vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-      vim.api.nvim_set_keymap("i", "<C-H>", "copilot#Previous()", { silent = true, expr = true })
-      vim.api.nvim_set_keymap("i", "<C-L>", "copilot#Next()", { silent = true, expr = true })
-    end,
-  },
+  -- {
+  --   "github/copilot.vim",
+  --   init = function(_)
+  --     vim.g.copilot_no_tab_map = true
+  --     vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+  --     vim.api.nvim_set_keymap("i", "<C-H>", "copilot#Previous()", { silent = true, expr = true })
+  --     vim.api.nvim_set_keymap("i", "<C-L>", "copilot#Next()", { silent = true, expr = true })
+  --   end,
+  -- },
 }
