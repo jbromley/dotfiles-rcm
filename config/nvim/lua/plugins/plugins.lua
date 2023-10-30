fd97cbfc86bf82260e18061388ee6af33482cfd5
J. Bromley <jay@appliedinvention.com>
2023-10-30 13:01:49 -0700

    Cleaned up Neovim plugins.lua
    Removed unused, commented code and standardized plugin blocks to all use
    {}.

--- config/nvim/lua/plugins/plugins.lua
+++ config/nvim/lua/plugins/plugins.lua
@@ -1,6 +1,10 @@
 return {
-  "tpope/vim-commentary",
-  "tpope/vim-fugitive",
+  {
+    "tpope/vim-commentary",
+  },
+  {
+    "tpope/vim-fugitive",
+  },
   {
     "vimwiki/vimwiki",
     delay = false,
@@ -17,13 +21,4 @@ return {
   {
     "gleam-lang/gleam.vim",
   },
-  -- {
-  --   "github/copilot.vim",
-  --   init = function(_)
-  --     vim.g.copilot_no_tab_map = true
-  --     vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-  --     vim.api.nvim_set_keymap("i", "<C-H>", "copilot#Previous()", { silent = true, expr = true })
-  --     vim.api.nvim_set_keymap("i", "<C-L>", "copilot#Next()", { silent = true, expr = true })
-  --   end,
-  -- },
 }
