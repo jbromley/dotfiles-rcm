c85061c32a47f61ace7ea893db2ce74f8ce64610
J. Bromley <jay@appliedinvention.com>
2024-01-05 17:03:30 -0800

    Cleaned up LSP configuration
    NvChad will properly map LSP executables names to the Mason directory if
    they used default configurations. Removed explicit line for
    racket_langserver.

--- config/nvim/lua/custom/configs/lspconfig.lua
+++ config/nvim/lua/custom/configs/lspconfig.lua
@@ -1,10 +1,11 @@
+local mason_bin = vim.fn.stdpath('data') .. "/mason/bin/"
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "clangd", "erlangls", "gleam"}
+local servers = { "clangd", "erlangls", "gleam", "racket_langserver"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -18,9 +19,7 @@ end
 
 lspconfig.elixirls.setup {
   -- cmd = { "/home/jay/.local/share/rtx/installs/elixir-ls/0.18.1/language_server.sh" },
-  cmd = { "/home/jay/.local/share/nvim/mason/packages/elixir-ls/language_server.sh", };
+  cmd = { mason_bin .. "elixir-ls", };
   on_attach = on_attach,
   capabilities = capabilities,
 }
-
-lspconfig.racket_langserver.setup { }
