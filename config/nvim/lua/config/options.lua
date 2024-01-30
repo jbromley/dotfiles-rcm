32797b2f652ce712eb5bd1c72aebd0607038e333
J. Bromley <jay@appliedinvention.com>
2024-01-30 14:24:55 -0800

    Make Neovide transparent.

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,4 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.guifont = "JetBrainsMono Nerd Font:h9"
+local alpha = function()
+  return string.format("%x", math.floor((255 * vim.g.transparency) or 0.875))
+end
+
+vim.g.neovide_transparency = 0.9375
+vim.g.neovide_window_blurred = true
+
+vim.opt.guifont = "JetBrainsMono Nerd Font:h10"
