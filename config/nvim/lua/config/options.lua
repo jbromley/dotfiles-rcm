6bf3070934aa1fe264dac5c24e9c2fc7e87df668
J. Bromley <jay@appliedinvention.com>
2024-01-30 17:04:33 -0800

    Made Neovide less transparent
    Set alpha to 31/32.

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -5,7 +5,7 @@ local alpha = function()
   return string.format("%x", math.floor((255 * vim.g.transparency) or 0.875))
 end
 
-vim.g.neovide_transparency = 0.9375
+vim.g.neovide_transparency = 0.96875
 vim.g.neovide_window_blurred = true
 
 vim.opt.guifont = "JetBrainsMono Nerd Font:h10"
