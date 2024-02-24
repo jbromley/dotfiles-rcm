8ddc5c0da202d0be32be7b87c42ebf80f3cd8eb3
J. Bromley <jay@appliedinvention.com>
2024-02-23 18:02:47 -0800

    Fix node path for Neovim GUI clients
    The home directory was hard-coded into the path for the node binary, so
    this didn't work on machines where the user name was not "jay". Fixed to
    use the `HOME` environment variable to set the path to `node`.

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -10,7 +10,7 @@ vim.g.neovide_window_blurred = true
 
 -- Some LSPs use node. Make sure it is in the path.
 if vim.fn.executable("node") == 0 then
-  vim.env.PATH = vim.env.PATH .. ":/home/jay/.local/share/mise/installs/node/20/bin"
+  vim.env.PATH = vim.env.PATH .. ":" .. vim.env.HOME .. "/.local/share/mise/installs/node/20/bin"
 end
 
 -- Set the font for GUI clients.
