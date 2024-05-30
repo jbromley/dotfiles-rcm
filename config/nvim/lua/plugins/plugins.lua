4a90b30bb6223d7986cd22e873cc95b0b211f9ff
J. Bromley <jay@appliedinvention.com>
2024-05-29 17:45:34 -0700

    Use Neogit plugin in Neovim
    This is the neovim equivalent of Emacs' magit.

--- config/nvim/lua/plugins/plugins.lua
+++ config/nvim/lua/plugins/plugins.lua
@@ -17,6 +17,18 @@ return {
     },
   },
   {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+      -- "ibhagwan/fzf-lua", -- optional
+    },
+    config = true,
+  },
+  {
     "nvim-neorg/neorg",
     version = "v7.0.0",
     dependencies = { "nvim-lua/plenary.nvim" },
