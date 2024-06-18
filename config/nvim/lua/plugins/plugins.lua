a56ef4dd027984f4b1698b022b9a0e3f96b2ea03
J. Bromley <jay@appliedinvention.com>
2024-06-17 18:46:56 -0700

    Add nvim-orgmode to Neovim configuration

--- config/nvim/lua/plugins/plugins.lua
+++ config/nvim/lua/plugins/plugins.lua
@@ -53,4 +53,25 @@ return {
       })
     end,
   },
+  {
+    "nvim-orgmode/orgmode",
+    event = "VeryLazy",
+    ft = { "org" },
+    config = function()
+      -- Setup orgmode
+      require("orgmode").setup({
+        org_agenda_files = "~/Org/**/*",
+        org_default_notes_file = "~/Org/notes.org",
+        org_todo_keywords = { "TODO(t)", "IN PROGRESS(i)", "|", "DONE(d)" },
+        org_hide_leading_stars = true,
+      })
+
+      -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+      -- add ~org~ to ignore_install
+      -- require('nvim-treesitter.configs').setup({
+      --   ensure_installed = 'all',
+      --   ignore_install = { 'org' },
+      -- })
+    end,
+  },
 }
