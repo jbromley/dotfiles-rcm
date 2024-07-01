3417449c4d8dc447066b022103e95ced9acc274d
J. Bromley <jay@appliedinvention.com>
2024-07-01 11:46:06 -0700

    Replace neorg with nvim-orgmode

--- config/nvim/lua/plugins/plugins.lua
+++ config/nvim/lua/plugins/plugins.lua
@@ -29,31 +29,6 @@ return {
     config = true,
   },
   {
-    "nvim-neorg/neorg",
-    version = "v7.0.0",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    build = ":Neorg sync-parsers",
-    -- tag = "*",
-    lazy = true,
-    ft = "norg",
-    cmd = "Neorg",
-    config = function()
-      require("neorg").setup({
-        load = {
-          ["core.defaults"] = {},
-          ["core.concealer"] = { config = { icons = { todo = { undone = { icon = " " } } } } },
-          ["core.dirman"] = {
-            config = {
-              workspaces = {
-                notes = "~/Documents/notes",
-              },
-            },
-          },
-        },
-      })
-    end,
-  },
-  {
     "nvim-orgmode/orgmode",
     event = "VeryLazy",
     ft = { "org" },
