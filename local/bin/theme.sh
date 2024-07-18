dcdd6c52733436ef4cda369e16b652bac210c7fa
J. Bromley <jay@appliedinvention.com>
2024-07-17 21:13:59 -0700

    Created unified theme-changing script

---
+++ local/bin/theme.sh
@@ -0,0 +1,42 @@
+#! /usr/bin/env bash
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+
+usage() {
+  echo "Usage: $(basename $0) THEME"
+  echo "   THEME is light or dark"
+}
+
+if [ "$#" != "1" ]; then
+  echo "Error: must specify theme"
+  usage "$0"
+  exit 1
+fi
+
+light_theme() {
+  kitty +kitten themes --config-file-name=themes.conf Catppuccin-Latte
+  ln -sf ~/.dotfiles/config/bat/config-light ~/.config/bat/config
+  ln -sf ~/.dotfiles/config/nvim/lua/plugins/light-colorscheme.lua ~/.config/nvim/lua/plugins/current-colorscheme.lua
+}
+
+dark_theme() {
+  kitty +kitten themes --config-file-name=themes.conf Dracula
+  ln -sf ~/.dotfiles/config/bat/config-dark ~/.config/bat/config
+  ln -sf ~/.dotfiles/config/nvim/lua/plugins/dark-colorscheme.lua ~/.config/nvim/lua/plugins/current-colorscheme.lua
+}
+
+case "$1" in
+light)
+  light_theme
+  ;;
+dark)
+  dark_theme
+  ;;
+*)
+  echo "Unknown theme: $1"
+  usage "$0"
+  exit 2
+  ;;
+esac
