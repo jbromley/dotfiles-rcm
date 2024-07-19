852d827a342f1b63f5b9de0ac8b030724a5ec7df
J. Bromley <jay@appliedinvention.com>
2024-07-18 19:09:17 -0700

    Removed dead code
    Was no longer using function to calculate display alpha.

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,10 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-local alpha = function()
-  return string.format("%x", math.floor((255 * vim.g.transparency) or 0.875))
-end
-
 vim.g.neovide_transparency = 0.96875
 vim.g.neovide_window_blurred = true
 
