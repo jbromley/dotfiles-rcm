e6b820bca9a446b765b5f42a8fb42af6e8875d2e
J. Bromley <jay@appliedinvention.com>
2024-08-05 14:41:13 -0700

    Remove manual setting of `node` path
    This is now handled by the mise.nvim plugin.

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -4,10 +4,5 @@
 vim.g.neovide_transparency = 0.96875
 vim.g.neovide_window_blurred = true
 
--- Some LSPs use node. Make sure it is in the path.
-if vim.fn.executable("node") == 0 then
-  vim.env.PATH = vim.env.PATH .. ":" .. vim.env.HOME .. "/.local/share/mise/installs/node/20/bin"
-end
-
 -- Set the font for GUI clients.
 vim.opt.guifont = "Hack Nerd Font:h10"
