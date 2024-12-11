23c8cbc5f982bc34e3500e3b48071af0adbb7dd2
J. Bromley <jbromley@gmail.com>
2024-12-10 17:52:33 -0800

    Set Dracula background color in pre-early-init.el

--- emacs.d/pre-early-init.el
+++ emacs.d/pre-early-init.el
@@ -1,5 +1,7 @@
 ;;; pre-early-init.el --- Run before body of early-init.el -*- no-byte-compile: t; lexical-binding: t; -*-
 
+(setq default-frame-alist '((background-color . "#282a36")))
+
 ;; Reducing clutter in ~/.emacs.d by redirecting files to ~/emacs.d/var/
 (setq minimal-emacs-var-dir (expand-file-name "var/" minimal-emacs-user-directory))
 (setq package-user-dir (expand-file-name "elpa" minimal-emacs-var-dir))
